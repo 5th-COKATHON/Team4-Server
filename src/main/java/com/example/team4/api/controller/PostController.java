fbb0d5bcad380af823a6c0caba447575ee93bb81
gikhoon <ghikhoon@naver.com>
2025-01-18 02:32:43 +0900

    feat: 게시물 저장 API

--- src/main/java/com/example/team4/api/controller/PostController.java
+++ src/main/java/com/example/team4/api/controller/PostController.java
@@ -2,6 +2,7 @@ package com.example.team4.api.controller;
 
 import com.example.team4.domain.service.PostService;
 import com.example.team4.api.dto.request.AddPostRequest;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostController {
     private final PostService postService;
 
+    @Operation(summary = "일기 등록", description = "일기 등록")
     @PostMapping
     public ResponseEntity<Void> addPosts(@Valid @RequestBody AddPostRequest request) {
         postService.addPost(request.memberId(), request.dateTime(), request.content(), request.emotion(),
