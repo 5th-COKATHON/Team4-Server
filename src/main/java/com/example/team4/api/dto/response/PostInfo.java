72eb1227845498cb0eec479971fae962e3a3135a
gikhoon <ghikhoon@naver.com>
2025-01-18 02:40:46 +0900

    feat: postInfo에 날짜 추가

--- src/main/java/com/example/team4/api/dto/response/PostInfo.java
+++ src/main/java/com/example/team4/api/dto/response/PostInfo.java
@@ -2,6 +2,7 @@ package com.example.team4.api.dto.response;
 
 import com.example.team4.domain.Emotion;
 import com.example.team4.domain.entity.Post;
+import java.time.LocalDate;
 
 public record PostInfo(
         Long postId,
@@ -9,10 +10,11 @@ public record PostInfo(
         Double longitude,
         String placeName,
         String context,
-        Emotion emotion
+        Emotion emotion,
+        LocalDate date
 ) {
     public static PostInfo from(Post post) {
         return new PostInfo(post.getId(), post.getLatitude(), post.getLongitude(), post.getPlaceName(),
-                post.getContext(), post.getEmotion());
+                post.getContext(), post.getEmotion(), post.getDate());
     }
 }
